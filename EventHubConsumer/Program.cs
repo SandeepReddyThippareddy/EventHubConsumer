c183dcc53370bd4fc127e7354a70bfa9b917e537
Sandeep Reddy <sandeep.thippareddy.ext@zeiss.com>
2023-04-10 19:30:40 +0530

    Add project files.

---
+++ EventHubConsumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+
+var connectionString = "";
+var consumerGroup = "$Default";
+
+
+EventHubConsumerClient eventHubConsumerClient= new EventHubConsumerClient(consumerGroup, connectionString);
+
+await foreach(PartitionEvent _event in eventHubConsumerClient.ReadEventsAsync())
+{
+    Console.WriteLine(_event.Partition.PartitionId);
+    Console.WriteLine(_event.Data.Offset);
+    Console.WriteLine(_event.Data.SequenceNumber);
+    Console.WriteLine(_event.Data.PartitionKey);
+    Console.WriteLine(_event.Data.EventBody);
+}
+
+Console.ReadKey();
